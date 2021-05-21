22283ef0e44390e1788752388c35f39ad6f4536f
Hedtke, Moritz <Moritz.Hedtke@t-online.de>
2021-05-21 22:12:29 +0200

    Improve form validation

--- src/routes/users/create.svelte
+++ src/routes/users/create.svelte
@@ -4,7 +4,8 @@ SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 -->
 <script lang="ts">
 	let type = "voter";
-    let feedback: any = {}
+    let feedback: Map<string, string> = new Map()
+    let unknownFeedback = []
 
     async function createUser() {
         let formData = new FormData(document.querySelector("#form-users"))
@@ -12,7 +13,13 @@ SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
             method: "POST",
             body: formData
         })
-        feedback = await reponse.json()
+        feedback = new Map(Object.entries(await reponse.json()))
+        console.log(feedback)
+    }
+
+    $: {
+        console.log("update")
+        unknownFeedback = [...feedback.entries()].filter(e => !["name"].includes(e[0]))
     }
 </script>
 
@@ -25,12 +32,20 @@ SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 <div class="row justify-content-center">
 	<div class="col-md-7 col-lg-8">
 		<form on:submit|preventDefault={createUser} id="form-users">
+            {#if unknownFeedback.length != 0}
+                <div class="alert alert-danger" role="alert">
+                    {#each unknownFeedback as [attribute, message]}
+                        {attribute}: {message}<br>
+                    {/each}
+                </div>
+            {/if}
+
 			<div class="mb-3">
 				<label for="users-name" class="form-label">Name:</label>
-				<input type="text" class="form-control {"name" in feedback ? "is-invalid" : ""}" name="name" id="users-name" aria-describedby="users-name-feedback" />
-                {#if "name" in feedback}
+				<input type="text" class="form-control {feedback.has("name") ? "is-invalid" : ""}" name="name" id="users-name" aria-describedby="users-name-feedback" />
+                {#if feedback.has("name")}
                 <div id="users-name-feedback" class="invalid-feedback">
-                    {feedback.name}
+                    {feedback.get("name")}
                   </div>
                 {/if}
 			</div>
@@ -40,7 +55,7 @@ SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 			</div>
 			<div class="mb-3">
 				<label for="users-type" class="form-label">Nutzerart:</label>
-				<select required bind:value={type} class="form-select" name="type" id="users-type">
+				<select bind:value={type} class="form-select" name="type" id="users-type">
 					<option selected value="voter">Schüler</option>
 					<option value="helper">Helfer</option>
 					<option value="admin">Admin</option>
@@ -49,11 +64,11 @@ SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 			{#if type === 'voter'}
 				<div class="mb-3">
 					<label for="users-group" class="form-label">Klasse:</label>
-					<input required type="text" class="form-control" name="group" id="users-group" />
+					<input type="text" class="form-control" name="group" id="users-group" />
 				</div>
                 <div class="mb-3">
 					<label for="users-age" class="form-label">Jahrgang:</label>
-					<input required type="text" class="form-control" name="age" id="users-age" />
+					<input type="text" class="form-control" name="age" id="users-age" />
 				</div>
 			{/if}
 			<div class="mb-3 form-check">
