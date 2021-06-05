a7a321bc03b7582e4eb6a6405f09e7ce629468ef
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-06-05 19:38:07 +0200

    Also migrate projects to the new template literals

--- src/routes/projects/index.json.ts
+++ src/routes/projects/index.json.ts
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 import { buildGet } from '$lib/list-entities';
+import { fakeTT } from '$lib/tagged-templates';
 
 export const get = buildGet(
 	[
@@ -14,16 +15,27 @@ export const get = buildGet(
 		'max_participants',
 		'random_assignments'
 	],
-	'id,title,info,place,costs,min_age,max_age,min_participants,max_participants,presentation_type,requirements,random_assignments',
+	[
+		'id',
+		'title',
+		'info',
+		'place',
+		'costs',
+		'min_age',
+		'max_age',
+		'min_participants',
+		'max_participants',
+		'presentation_type',
+		'requirements',
+		'random_assignments'
+	],
 	'projects',
-	'title LIKE $5 AND ($6 OR id = $7) AND info LIKE $8 AND place LIKE $9 AND presentation_type LIKE $10 AND requirements LIKE $11',
-	(query) => [
-		'%' + (query.get('filter_title') ?? '') + '%', // $5
-		!query.has('filter_id'), // $6
-		query.get('filter_id'), // $7 // TODO FIXME if this is "" we 500
-		'%' + (query.get('filter_info') ?? '') + '%', // $8
-		'%' + (query.get('filter_place') ?? '') + '%', // $9
-		'%' + (query.get('filter_presentation_type') ?? '') + '%', // $10
-		'%' + (query.get('filter_requirements') ?? '') + '%' // $11
-	]
+	(query) =>
+		fakeTT`title LIKE ${'%' + (query.get('filter_title') ?? '') + '%'} AND (${!query.has(
+			'filter_id'
+		)} OR id = ${query.get('filter_id')}) AND info LIKE ${
+			'%' + (query.get('filter_info') ?? '') + '%'
+		} AND place LIKE ${'%' + (query.get('filter_place') ?? '') + '%'} AND presentation_type LIKE ${
+			'%' + (query.get('filter_presentation_type') ?? '') + '%'
+		} AND requirements LIKE ${'%' + (query.get('filter_requirements') ?? '') + '%'}`
 );
