9de90c851db432f50813840d521d5c404901c8a1
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-01 21:32:31 +0200

    Fix

--- src/lib/EntityList.svelte
+++ src/lib/EntityList.svelte
@@ -2,7 +2,7 @@
 SPDX-License-Identifier: AGPL-3.0-or-later
 SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 -->
-<script lang="typescript">
+<script lang="ts">
 	import { query as query2 } from './writable_url';
 	import type { Writable } from 'svelte/store';
 	import type { BaseEntityType, BaseQueryType, EntityResponseBody } from './entites';
