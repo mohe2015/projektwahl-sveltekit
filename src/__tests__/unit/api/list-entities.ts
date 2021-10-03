6db2c7dcdd750ce9c05f68a202d7639a917cfec1
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-03 23:27:32 +0200

    Start fixing tests

--- src/__tests__/unit/api/list-entities.ts
+++ src/__tests__/unit/api/list-entities.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 import 'jest';
-import fetch, { BodyInit, Response } from 'node-fetch';
+import fetch, { Response } from 'node-fetch';
 import type { TestResponseBody } from 'src/routes/tests/list-entities.json';
 import { fetchPost } from '../../../test_utils';
 
--- src/test_utils.ts
+++ src/test_utils.ts
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
+import fetch, { RequestInit } from 'node-fetch';
 
 export const fetchPost = async (url: string, options: RequestInit | undefined) => {
 	const response = await fetch(url, options);
