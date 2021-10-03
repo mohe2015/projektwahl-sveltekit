df4df02cc90c129351d8847378bdd9274bcca17d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-03 21:31:10 +0200

    Combine import and creation

--- src/routes/users/create-or-update.json.ts
+++ src/routes/users/create-or-update.json.ts
@@ -3,12 +3,80 @@
 import { allowUserType, checkPermissions } from '$lib/authorization';
 import { sql } from '$lib/database';
 import { hashPassword } from '$lib/password';
+import type { UserType } from '$lib/types';
 import type { EndpointOutput, RequestHandler } from '@sveltejs/kit/types/endpoint';
 import type { JSONValue } from '@sveltejs/kit/types/helper';
 import type { PostgresError } from 'postgres';
 import type { MyLocals } from 'src/hooks';
 import type { CreateResponse } from '../projects/create-or-update.json';
 import { permissions } from './permissions';
+
+export const save = async (
+	data: AsyncIterable<JSONValue>,
+	loggedInUser: UserType | null
+): Promise<EndpointOutput<CreateResponse>> => {
+	return await sql.begin('READ WRITE', async (sql) => {
+		for await (const entry of data) {
+			const user = checkPermissions(permissions, loggedInUser, entry);
+
+			try {
+				// TODO FIXME allow helper to change this but only specific fields (NOT type)
+				if (user.id !== undefined) {
+					const [row] = await sql`UPDATE users SET
+	name = CASE WHEN ${user.name !== undefined} THEN ${user.name ?? null} ELSE name END,
+	password_hash = CASE WHEN ${user.password !== undefined} THEN ${
+						user.password ? await hashPassword(user.password) : null
+					} ELSE password_hash END,
+	type = CASE WHEN ${user.type !== undefined} THEN ${user.type ?? null} ELSE type END,
+	"group" = CASE WHEN ${user.group !== undefined} THEN ${user.group ?? null} ELSE "group" END,
+	age = CASE WHEN ${user.age /*for csv import*/} THEN ${user.age ?? null} ELSE age END,
+	away = CASE WHEN ${user.away !== undefined} THEN ${user.away ?? null} ELSE away END,
+	project_leader_id = CASE WHEN ${user.project_leader_id !== undefined} THEN ${
+						user.project_leader_id ?? null
+					} ELSE project_leader_id END
+	WHERE id = ${user.id!} RETURNING id;`;
+				} else {
+					const [row] =
+						await sql`INSERT INTO users (name, password_hash, type, "group", age, away) VALUES (${
+							user.name ?? null
+						}, ${user.password ? await hashPassword(user.password) : null}, ${user.type ?? null}, ${
+							user.group ?? null
+						}, ${user.age ? user.age : null /* for csv import */}, ${
+							user.away ?? false
+						}) RETURNING id;`;
+				}
+			} catch (error: unknown) {
+				console.log(error);
+				if (error instanceof Error && error.name === 'PostgresError') {
+					const postgresError = error as PostgresError;
+					if (
+						postgresError.code === '23505' &&
+						postgresError.constraint_name === 'users_name_key'
+					) {
+						// unique violation
+						const response: EndpointOutput<CreateResponse> = {
+							body: {
+								errors: {
+									name: 'Nutzer mit diesem Namen existiert bereits!'
+								}
+							}
+						};
+						return response;
+					}
+				}
+				const response: EndpointOutput<CreateResponse> = {
+					status: 500
+				};
+				return response;
+			}
+		}
+		return {
+			body: {
+				errors: {}
+			}
+		};
+	});
+};
 
 // TODO FIXME these could probably also be generalized by adding mapper functions (e.g. for the password here)
 export const post: RequestHandler<MyLocals, JSONValue> = async function (
@@ -17,63 +85,8 @@ export const post: RequestHandler<MyLocals, JSONValue> = async function (
 	allowUserType(request, ['admin']);
 	const { body } = request;
 
+	return await save([body], request.locals.user);
+
 	// TODO FIXME when implementing update don't accidentially destroy the password
 	// TODO FIXME if you erase the field you get an empty password - we need a way to reset the password and one to leave it like it was
-
-	const user = checkPermissions(permissions, request.locals.user, body);
-
-	try {
-		// TODO FIXME allow helper to change this but only specific fields (NOT type)
-		const [row] = await sql.begin('READ WRITE', async (sql) => {
-			if (user.id !== undefined) {
-				return await sql`UPDATE users SET
-name = CASE WHEN ${user.name !== undefined} THEN ${user.name ?? null} ELSE name END,
-password_hash = CASE WHEN ${user.password !== undefined} THEN ${
-					user.password ? await hashPassword(user.password) : null
-				} ELSE password_hash END,
-type = CASE WHEN ${user.type !== undefined} THEN ${user.type ?? null} ELSE type END,
-"group" = CASE WHEN ${user.group !== undefined} THEN ${user.group ?? null} ELSE "group" END,
-age = CASE WHEN ${user.age !== undefined} THEN ${user.age ?? null} ELSE age END,
-away = CASE WHEN ${user.away !== undefined} THEN ${user.away ?? null} ELSE away END,
-project_leader_id = CASE WHEN ${user.project_leader_id !== undefined} THEN ${
-					user.project_leader_id ?? null
-				} ELSE project_leader_id END
-WHERE id = ${user.id!} RETURNING id;`;
-			} else {
-				return await sql`INSERT INTO users (name, password_hash, type, "group", age, away) VALUES (${
-					user.name ?? null
-				}, ${user.password ? await hashPassword(user.password) : null}, ${user.type ?? null}, ${
-					user.group ?? null
-				}, ${user.age ?? null}, ${user.away ?? false}) RETURNING id;`;
-			}
-		});
-
-		const response: EndpointOutput<CreateResponse> = {
-			body: {
-				errors: {},
-				id: row.id
-			}
-		};
-		return response;
-	} catch (error: unknown) {
-		if (error instanceof Error && error.name === 'PostgresError') {
-			const postgresError = error as PostgresError;
-			if (postgresError.code === '23505' && postgresError.constraint_name === 'users_name_key') {
-				// unique violation
-				const response: EndpointOutput<CreateResponse> = {
-					body: {
-						errors: {
-							name: 'Nutzer mit diesem Namen existiert bereits!'
-						}
-					}
-				};
-				return response;
-			}
-		}
-		console.log(error);
-		const response: EndpointOutput<CreateResponse> = {
-			status: 500
-		};
-		return response;
-	}
 };
--- src/routes/users/import/create-or-update.json.ts
+++ src/routes/users/import/create-or-update.json.ts
@@ -11,6 +11,7 @@ import parse from 'csv-parse';
 import { Readable } from 'stream';
 import type { PostgresError } from 'postgres';
 import { permissions } from '../permissions';
+import { save } from '../create-or-update.json';
 
 export type UserImportRequest = { [x: string]: any; fileInput?: string; id?: number | undefined };
 
@@ -27,47 +28,5 @@ export const post: RequestHandler<MyLocals, UserImportRequest> = async function 
 		})
 	);
 
-	const response = await sql.begin('READ WRITE', async (sql) => {
-		// TODO FIXME do this in users and make create-or-update a many-update operatoin
-		for await (const body of parser) {
-			const user = checkPermissions(permissions, request.locals.user, body);
-
-			try {
-				await sql`INSERT INTO users (name, password_hash, type, "group", age, away) VALUES (${
-					user.name
-				}, ${user.password ? await hashPassword(user.password) : null}, ${user.type}, ${
-					user.group ?? null
-				}, ${user.age ? user.age : null}, ${user.away ?? false}) RETURNING id;`;
-			} catch (error: unknown) {
-				if (error instanceof Error && error.name === 'PostgresError') {
-					const postgresError = error as PostgresError;
-					if (
-						postgresError.code === '23505' &&
-						postgresError.constraint_name === 'users_name_key'
-					) {
-						// unique violation
-						const response: EndpointOutput<CreateResponse> = {
-							body: {
-								errors: {
-									name: 'Nutzer mit diesem Namen existiert bereits!'
-								}
-							}
-						};
-						return response;
-					}
-				}
-				const response: EndpointOutput<CreateResponse> = {
-					status: 500
-				};
-				return response;
-			}
-		}
-		return {
-			body: {
-				errors: {}
-			}
-		};
-	});
-
-	return response;
+	return await save(parser, request.locals.user);
 };
