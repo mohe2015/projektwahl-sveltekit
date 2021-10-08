f425734938ec4e226abb0a322d67868240fe838a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-08 20:11:23 +0200

    Jo

--- src/lib/FailureResult.svelte
+++ src/lib/FailureResult.svelte
@@ -1,15 +1,19 @@
+<!--
+SPDX-License-Identifier: AGPL-3.0-or-later
+SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
+-->
 <script lang="ts">
-    import { hasErrors } from "./error-handling";
-import type { Result } from "./types";
+	import { hasErrors } from './error-handling';
+	import type { Result } from './types';
 
-    export let result: Result<unknown>;
+	export let result: Result<unknown>;
 </script>
 
 {#if hasErrors(result)}
-    <div class="alert alert-danger" role="alert">
-        Fehler:<br />
-        {#each Object.entries(result.failure) as [attribute, message]}
-            {attribute}: {message}<br />
-        {/each}
-    </div>
+	<div class="alert alert-danger" role="alert">
+		Fehler:<br />
+		{#each Object.entries(result.failure) as [attribute, message]}
+			{attribute}: {message}<br />
+		{/each}
+	</div>
 {/if}
--- src/lib/error-handling.ts
+++ src/lib/error-handling.ts
@@ -1,47 +1,52 @@
-import type { Result } from "./types";
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
+
+import type { Result } from './types';
 
 export const myFetch = async <T>(
 	url: string,
 	options: RequestInit | undefined
 ): Promise<Result<T>> => {
-    try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            try {
-                const additionalInfo = await response.text();
-                return {
-                    failure: {
-                        network: `Failed to request ${url}: ${response.status} ${response.statusText}\nAdditional information: ${additionalInfo}`
-                    }
-                }
-            } catch (error: unknown) {
-                return {
-                    failure: {
-                        network: `Failed to request ${url}: ${response.status} ${response.statusText}`
-                    }
-                }
-            }
-        }
-        const result = (await response.json()) as Result<T>;
-        return result;
-    } catch (error) {
-        console.error(error);
-        if (error instanceof TypeError) {
-            return {
-                failure: {
-                    network: `Failed to request ${url}: ${error.message}\nAdditional information: ${error.stack ?? 'none'}`
-                }
-            }
-        } else {
-            return {
-                failure: {
-                    network: `Failed to request ${url}: Unknown error see console.`
-                }
-            }
-        }
-    }
+	try {
+		const response = await fetch(url, options);
+		if (!response.ok) {
+			try {
+				const additionalInfo = await response.text();
+				return {
+					failure: {
+						network: `Failed to request ${url}: ${response.status} ${response.statusText}\nAdditional information: ${additionalInfo}`
+					}
+				};
+			} catch (error: unknown) {
+				return {
+					failure: {
+						network: `Failed to request ${url}: ${response.status} ${response.statusText}`
+					}
+				};
+			}
+		}
+		const result = (await response.json()) as Result<T>;
+		return result;
+	} catch (error) {
+		console.error(error);
+		if (error instanceof TypeError) {
+			return {
+				failure: {
+					network: `Failed to request ${url}: ${error.message}\nAdditional information: ${
+						error.stack ?? 'none'
+					}`
+				}
+			};
+		} else {
+			return {
+				failure: {
+					network: `Failed to request ${url}: Unknown error see console.`
+				}
+			};
+		}
+	}
 };
 
 export const hasErrors = (result: Result<unknown>): boolean => {
-    return Object.keys(result.failure).length !== 0
-}
+	return Object.keys(result.failure).length !== 0;
+};
