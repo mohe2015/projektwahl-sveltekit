f6c7c477b15bd9483fa17439bbd9280d9ea7780a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-12 21:35:24 +0200

    Jo

--- src/routes/users/import/create-or-update.json.ts
+++ src/routes/users/import/create-or-update.json.ts
@@ -2,10 +2,12 @@
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 import type { EndpointOutput, RequestHandler } from '@sveltejs/kit/types/endpoint';
 import type { MyLocals } from 'src/hooks';
-import type { CreateResponse } from '../../projects/create-or-update.json';
 import parse from 'csv-parse';
 import { Readable } from 'stream';
 import { save } from '../create-or-update.json';
+import {
+	TransformStream
+  } from 'node:stream/web';
 
 export type UserImportRequest = { fileInput?: string; id: number };
 
@@ -17,16 +19,19 @@ export const post: RequestHandler<MyLocals, UserImportRequest> = async function 
 	if (!request.body.fileInput) {
 		return {
 			status: 400
-		};
+		};g
 	}
+
+	const TransformStream = new TransformStream();
 
 	const parser = Readable.from(request.body.fileInput).pipe(
 		parse({
 			trim: true,
 			columns: true,
-			delimiter: ';'
+			delimiter: ';',
+			cast: true,
 		})
-	);
+	).pipe();
 
 	return await save(parser, request.locals.user);
 };
